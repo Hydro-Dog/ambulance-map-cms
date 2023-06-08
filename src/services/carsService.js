10b08a213d86f40678381d14382c36ad9dc8d44b
Vlad Babaev <vladbabaev11@gmail.com>
2023-06-08 19:22:01 +0500

    second

--- src/services/carsService.js
+++ src/services/carsService.js
@@ -4,8 +4,8 @@ export const getCarsData = async () => {
   return getGeolocation().then((coords) => {
     const data = Array.from({ length: 7 }).map((_, index) => ({
         id: Math.random()*1000,
-        latitude: coords.latitude + Math.random() * 0.05,
-        longitude: coords.longitude + Math.random() * 0.05,
+        latitude: coords.latitude + Math.random() * 0.03,
+        longitude: coords.longitude + Math.random() * 0.03,
         name: "Бригада " + (index + 1),
     }));
     return data;
--- src/services/consumerService.js
+++ src/services/consumerService.js
@@ -2,8 +2,8 @@ import { getGeolocation } from "./locationService.js";
 
 export const getConsumerData = () => {
   return getGeolocation().then((coords) => ({
-    latitude: coords.latitude + Math.random() * 0.05,
-    longitude: coords.longitude + Math.random() * 0.05,
+    latitude: coords.latitude + Math.random() * 0.01,
+    longitude: coords.longitude + Math.random() * 0.01,
     name: "Александр Сергеевич Иванов",
     age: 46,
     phoneNumber: "770-975-44-23",
