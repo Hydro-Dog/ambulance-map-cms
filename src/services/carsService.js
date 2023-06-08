82057b49ae60c37b4e479dd5f396626a69c583d8
Vlad Babaev <vladbabaev11@gmail.com>
2023-06-08 19:26:10 +0500

    third

--- src/services/carsService.js
+++ src/services/carsService.js
@@ -4,8 +4,8 @@ export const getCarsData = async () => {
   return getGeolocation().then((coords) => {
     const data = Array.from({ length: 7 }).map((_, index) => ({
         id: Math.random()*1000,
-        latitude: coords.latitude + Math.random() * 0.03,
-        longitude: coords.longitude + Math.random() * 0.03,
+        latitude: coords.latitude + Math.random() * 0.03 * (Math.random() > 0.5 ? 1 : -1),
+        longitude: coords.longitude + Math.random() * 0.03 * (Math.random() > 0.5 ? 1 : -1),
         name: "Бригада " + (index + 1),
     }));
     return data;
